b83fee16c6d7944415bb9220c674ecd3d72c9e52
nqhuyD <huynguyennbk@gmail.com>
2019-11-01 09:34:30 +0700

    Update Env MySQL Config

--- src/db/config.ts
+++ src/db/config.ts
@@ -3,17 +3,44 @@ import { ConnectionOptions } from 'typeorm';
 
 import '../helpers/env';
 
+interface MySQLConfig {
+  [key: string]: ConnectionOptions;
+}
+
 export const mongoConfig = {
   mongoURL: process.env.MONGO_URL,
 };
 
-export const mysqlConfig: ConnectionOptions = {
+const baseMySqlConfig: ConnectionOptions = {
   type: 'mysql',
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB_NAME,
-  entities: ['../entities/*.ts'],
   synchronize: true,
+  logging: true,
+  entities: ['../entities/*.ts'],
+};
+
+export const mysqlConfig: MySQLConfig = {
+  development: {
+    ...baseMySqlConfig,
+    host: process.env.MYSQL_HOST_DEV,
+    port: process.env.MYSQL_PORT_DEV,
+    username: process.env.MYSQL_USER_DEV,
+    password: process.env.MYSQL_PASSWORD_DEV,
+    database: process.env.MYSQL_DB_NAME_DEV,
+  },
+  test: {
+    ...baseMySqlConfig,
+    host: process.env.MYSQL_HOST_TEST,
+    port: process.env.MYSQL_PORT_TEST,
+    username: process.env.MYSQL_USER_TEST,
+    password: process.env.MYSQL_PASSWORD_TEST,
+    database: process.env.MYSQL_DB_NAME_TEST,
+  },
+  production: {
+    ...baseMySqlConfig,
+    host: process.env.MYSQL_HOST_PROD,
+    port: process.env.MYSQL_PORT_PROD,
+    username: process.env.MYSQL_USER_PROD,
+    password: process.env.MYSQL_PASSWORD_PROD,
+    database: process.env.MYSQL_DB_NAME_PROD,
+  },
 };
--- src/db/mysqlORM.ts
+++ src/db/mysqlORM.ts
@@ -2,11 +2,14 @@ import { createConnection } from 'typeorm';
 
 import { mysqlConfig } from './config';
 import logger from '../utils/logger';
+import '../helpers/env';
 
 export default class MysqlORM {
   static async connect() {
     try {
-      await createConnection(mysqlConfig);
+      const connection = await createConnection(
+        mysqlConfig[process.env.NODE_ENV || 'development'],
+      );
       logger({ type: 'Notify', message: 'Connected To MySQL' });
     } catch (err) {
       logger({ type: 'Error', message: `Fail To Connect To MySQL ${err}` });
